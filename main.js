a36151f5f927b7595e756ab9a22b5b1204c5e285
Joao Andrade <joaovictorvieira.23@hotmail.com>
2020-12-29 21:42:36 -0300

    feat: add reload and devtools

--- main.js
+++ main.js
@@ -5,6 +5,7 @@ const path = require('path')
 const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+let addWindow;
 
 // Ouvir se o aplicativo está pronto
 app.on('ready', function () {
@@ -17,11 +18,37 @@ app.on('ready', function () {
     slashes: true
   }));
 
+  // Saia do aplicativo quando estiver fechado
+  mainWindow.on('closed', function () {
+    app.quit();
+  })
+
   // Construir menu a partir do template
   const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
   // Inserir menu
   Menu.setApplicationMenu(mainMenu)
-})
+});
+
+// Lidar com criação da janela de adição
+function createAddWindow() {
+  addWindow = new BrowserWindow({
+    width: 300,
+    height: 200,
+    title: 'Add shopping list item'
+  });
+  // Carregar HTML dentro da janela
+  addWindow.loadURL(url.format({
+    pathname: path.join(__dirname, 'addWindow.html'),
+    protocol: 'file:',
+    slashes: true
+  }));
+
+  // Garbage collection
+  addWindow.on('close', function () {
+    addWindow = null;
+  });
+
+}
 
 // Criar template do menu
 const mainMenuTemplate = [
@@ -29,7 +56,10 @@ const mainMenuTemplate = [
     label: 'File',
     submenu: [
       {
-        label: 'Add Item'
+        label: 'Add Item',
+        click() {
+          createAddWindow();
+        }
       },
       {
         label: 'Clear Items'
@@ -44,3 +74,27 @@ const mainMenuTemplate = [
     ]
   }
 ];
+
+// Se mac, adicionar um objeto vazio no menu
+if (process.platform == 'darwin') {
+  mainMenuTemplate.unshift({});
+}
+
+// Adicionar item de ferramentas de desenvolvedor se não estiver em produção
+if (process.env.NODE_ENV !== 'production') {
+  mainMenuTemplate.push({
+    label: 'Developer Tools',
+    submenu: [
+      {
+        label: 'Toggle DevTools',
+        accelerator: process.platform == 'darwin' ? 'Command+I' : 'Ctrl+I',
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        }
+      },
+      {
+        role: 'reload'
+      }
+    ]
+  })
+}
